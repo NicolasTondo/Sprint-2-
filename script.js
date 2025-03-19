97392ccaa198bcf3a6dda4fcc50d7bf1a96bb03f
NicolasTondo <nicolastondo@gmail.com>
2025-03-19 14:20:46 -0300

    comentarios

---
+++ script.js
@@ -0,0 +1,36 @@
+// referencia para os elementos HTML
+const fromCurrency = document.getElementById("fromCurrency");
+const toCurrency = document.getElementById("toCurrency");
+const amount = document.getElementById("amount");
+const convertBtn = document.getElementById("convert");
+const resetBtn = document.getElementById("reset");
+const result = document.getElementById("result");
+
+// requisição ao https  
+// usa lista do json
+//fetch("https://api.frankfurter.app/currencies")
+    .then(response => response.json())
+
+    // uso de then para definir a data de cada moeda da API
+    .then(data => {
+        Object.keys(data).forEach(currency => {
+            fromCurrency.innerHTML += `<option value="${currency}">${currency}</option>`;
+            toCurrency.innerHTML += `<option value="${currency}">${currency}</option>`;
+        });
+    });
+
+convertBtn.addEventListener("click", () => {
+    const from = fromCurrency.value;
+    const to = toCurrency.value;
+    const value = amount.value;
+    
+    if (from === to) {
+        result.innerText = "Escolha moedas diferentes.";
+        return;
+    }
+});
+
+resetBtn.addEventListener("click", () => {
+    amount.value = "";
+    result.innerText = "Resultado: -";
+});
