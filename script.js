ce8bdcfcb6fca3e3e175e0810d9bfcdbe17ef3dc
NicolasTondo <nicolastondo@gmail.com>
2025-03-19 15:20:39 -0300

    mudanças js

--- script.js
+++ script.js
@@ -1,17 +1,14 @@
-// referencia para os elementos HTML
 const fromCurrency = document.getElementById("fromCurrency");
 const toCurrency = document.getElementById("toCurrency");
 const amount = document.getElementById("amount");
 const convertBtn = document.getElementById("convert");
 const resetBtn = document.getElementById("reset");
 const result = document.getElementById("result");
+const calcula = document.getElementById("calcula");
 
-// requisição ao https 
-// uso do arquivo csv com json  
-    fetch("https://economia.awesomeapi.com.br/xml/available")
+// Buscar 
+fetch("https://api.frankfurter.app/currencies")
     .then(response => response.json())
-
-    // uso de then para definir a data de cada moeda da API
     .then(data => {
         Object.keys(data).forEach(currency => {
             fromCurrency.innerHTML += `<option value="${currency}">${currency}</option>`;
@@ -19,17 +16,30 @@ const result = document.getElementById("result");
         });
     });
 
+//calcula.addEventListener(''
+
+//  moeda
 convertBtn.addEventListener("click", () => {
     const from = fromCurrency.value;
     const to = toCurrency.value;
     const value = amount.value;
+    if (!value) {
+        return false;
+    }
     
     if (from === to) {
         result.innerText = "Escolha moedas diferentes.";
         return;
     }
+    
+    fetch(`https://api.frankfurter.app/latest?amount=${value}&from=${from}&to=${to}`)
+        .then(response => response.json())
+        .then(data => {
+            result.innerText = `Resultado: ${data.rates[to]} ${to}`;
+        });
 });
 
+// Resetar valores
 resetBtn.addEventListener("click", () => {
     amount.value = "";
     result.innerText = "Resultado: -";
